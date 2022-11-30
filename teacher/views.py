c4b5c56ce18fb7f74217d0e640716ee3f28c0cdd
Jose Mauro <z.mauro@live.com>
2022-11-29 23:22:14 -0300

    Create Teacher view

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+from teacher.models import Teacher
+from teacher.serializers import TeacherSerializer
+
+
+class ProfessorAPIView(APIView):
+    def get(self, request, format=None):
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
